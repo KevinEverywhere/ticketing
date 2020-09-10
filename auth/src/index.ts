051d646a5beee84e1c1cec4ab3cc0f029ddb6fbd
Kevin Ready <kevin.ready@gmail.com>
2020-09-09 21:12:11 -0700

    small test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Still  testing...');
+  console.log('Still more testing...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
